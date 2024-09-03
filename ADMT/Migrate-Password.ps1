616e595ccb5ea9bdb82962fda9e4431eb3801cf5
fdelossantos <fdelossantos@users.noreply.github.com>
2024-09-03 14:31:15 +0200

    mejora migrate password

--- ADMT/Migrate-Password.ps1
+++ ADMT/Migrate-Password.ps1
@@ -1,3 +1,12 @@
+# Script para migrar usuarios con ADMT o para migrar las contraseñas cambiadas de usuarios ya migrados
+
+# Para migrar un usuario único
+# .\Migrate-UserWithPassword.ps1 -userName usuariodos -sourceDomain dominio.local -destinationDomain nuevodominio.local -targetOU "TestMig" -passwordServer DC01 -destServer SRV01
+
+# Para migrar un lote de usuarios
+# Crear un archivo con los nombres de usuario o identidades, uno por línea de archivo
+# .\Migrate-UserWithPassword.ps1 -userListFilePath C:\temp\usuarios.txt -sourceDomain dominio.local -destinationDomain nuevodominio.local -targetOU "TestMig" -passwordServer DC01 -destServer SRV01
+
 param (
     [Parameter(ParameterSetName = 'Multiple', Mandatory=$true, Position=0)]
     [string]$userListFilePath,
@@ -19,7 +28,11 @@ param (
 
     [Parameter(ParameterSetName = 'Multiple', Mandatory=$true)]
     [Parameter(ParameterSetName = 'Single', Mandatory=$true)]
-    [string]$passwordServer
+    [string]$passwordServer,
+
+    [Parameter(ParameterSetName = 'Multiple', Mandatory=$true)]
+    [Parameter(ParameterSetName = 'Single', Mandatory=$true)]
+    [string]$destServer
 
 )
 
@@ -27,11 +40,26 @@ $paramSet = $PSCmdlet.ParameterSetName
 
 if ($paramSet -eq 'Multiple') {
     Write-Output "Iniciando la migración para el usuario: $user"
-    ADMT USER /includefile:"$userListFilePath" /SD:"$sourceDomain" /TD:"$destinationDomain" /TO:"$targetOU" /migratesids:YES /passwordoption:copy /passwordserver:"$passwordServer" /disableoption:targetsameassource /migrategroups:yes /conflictoptions:merge
+    ADMT USER /includefile:"$userListFilePath" /SD:"$sourceDomain" /TD:"$destinationDomain" /TO:"$targetOU" /passwordoption:copy /passwordserver:"$passwordServer" /disableoption:targetsameassource /migrategroups:yes /conflictoptions:merge
+    
+    Write-Host "Esperando sincronización del dominio"
+    $users = Get-Content $userListFilePath
+    foreach ($user in $users) {
+        0..20 | foreach { Start-Sleep -Seconds 1; Write-Host "." -NoNewline } 
+        Write-Host ""
+
+        Set-ADUser -Server $destServer -ChangePasswordAtLogon $false -Identity $user
+        Write-Host "$user está listo."
+    }
+
     Write-Output "Migración completada para múltiples usuarios."
 }
 else {
     Write-Output "Iniciando la migración para el usuario: $userName"
-    ADMT USER /N "$userName" /SD:"$sourceDomain" /TD:"$destinationDomain" /TO:"$targetOU" /migratesids:YES /passwordoption:copy /passwordserver:"$passwordServer" /disableoption:targetsameassource /migrategroups:yes /conflictoptions:merge
+    ADMT USER /N "$userName" /SD:"$sourceDomain" /TD:"$destinationDomain" /TO:"$targetOU" /passwordoption:copy /passwordserver:"$passwordServer" /disableoption:targetsameassource /migrategroups:yes /conflictoptions:merge
+    Write-Host "Esperando sincronización del dominio"
+    0..20 | foreach { Start-Sleep -Seconds 1; Write-Host "." -NoNewline } 
+    Write-Host ""
+    Set-ADUser -Server $destServer -ChangePasswordAtLogon $false -Identity $userName
     Write-Output "Migración completada para el usuario: $userName"
-}
+} 
